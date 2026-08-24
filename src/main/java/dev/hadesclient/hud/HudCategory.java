634f8fca25e6d58eac8a4dc1bedec8a249ef7e63
HadesPower3241 <dragonbreath0305@gmail.com>
2026-08-23 23:10:08 -0700

    Delete src/main directory

--- src/main/java/dev/hadesclient/hud/HudCategory.java
+++
@@ -1,7 +0,0 @@
-package dev.hadesclient.hud;
-public enum HudCategory {
-    GENERAL("General"), COSMIC_PRISONS("Cosmic Prisons"), INPUT("Input"), SOCIAL("Social"), OTHER("Other");
-    private final String label;
-    HudCategory(String l) { label = l; }
-    public String label() { return label; }
-}
