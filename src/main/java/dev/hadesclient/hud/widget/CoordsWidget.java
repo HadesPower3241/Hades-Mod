cb2ca700d5b03fd41530a990bf5f70bfb4f266d3
HadesPower3241 <dragonbreath0305@gmail.com>
2026-08-23 18:24:00 -0700

    Add files via upload

---
+++ src/main/java/dev/hadesclient/hud/widget/CoordsWidget.java
@@ -0,0 +1,23 @@
+package dev.hadesclient.hud.widget;
+
+public final class CoordsWidget extends TextWidget {
+
+    public CoordsWidget() {
+        super("coords", "Coordinates");
+        defaults(Anchor.TOP_LEFT, 8f, 30f, true);
+    @Override public HudCategory category() { return HudCategory.GENERAL; }
+    }
+
+    @Override
+    protected String label() {
+        return "XYZ";
+    }
+
+    @Override
+    protected String value() {
+        if (mc().player == null) return "-";
+        return Math.round(mc().player.getX()) + ", "
+                + Math.round(mc().player.getY()) + ", "
+                + Math.round(mc().player.getZ());
+    }
+}
