7c25d748584ed4d6c05e32bc7806aec13b2dbef2
HadesPower3241 <dragonbreath0305@gmail.com>
2026-08-26 22:14:38 -0700

    Update GuardLineRenderLayer.java

--- src/main/java/dev/hadesclient/render/GuardLineRenderLayer.java
+++ src/main/java/dev/hadesclient/render/GuardLineRenderLayer.java
@@ -1,7 +1,27 @@
 package dev.hadesclient.render;
 
+import net.minecraft.client.render.VertexConsumer;
+import org.joml.Vector3f;
+
 public final class GuardLineRenderLayer {
 
     private GuardLineRenderLayer() {
+    }
+
+    public static void addVertex(
+            VertexConsumer consumer,
+            float x,
+            float y,
+            float z,
+            int red,
+            int green,
+            int blue,
+            int alpha,
+            float lineWidth
+    ) {
+        consumer.vertex(x, y, z)
+                .color(red, green, blue, alpha)
+                .normal(new Vector3f(0.0f, 1.0f, 0.0f))
+                .lineWidth(lineWidth);
     }
 }
